40859a48ca94a26a103f7c138afadf12054c16d7
TavrsD <dtavares@id.uff.br>
2025-03-05 01:01:32 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
 
 import { userRoutes } from "./http/controllers/users/routes";
 import { postRoutes } from "./http/controllers/posts/routes";
 
 export const app  = fastify()
+
+app.register(fastifyCors, {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+})
 
 app.register(userRoutes)
 app.register(postRoutes)
